840763cd51ff25389ca0273a83cb6c24e0cd8f1c
Alex Grant <alex.grant@shopify.com>
2024-08-20 11:31:53 -0400

    remove treesitter

--- init.lua
+++ init.lua
@@ -265,51 +265,7 @@ require('lazy').setup({
     dependencies = { 'tyru/open-browser.vim' }
   },
   { 'whiteinge/diffconflicts' },
-  {
-    'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
-    opts = {
-      ensure_installed = {
-        "lua",
-        "vim",
-        "vimdoc",
-        "markdown",
-        "markdown_inline",
-        "typescript",
-        "javascript",
-        "tsx",
-        "ruby"
-      },
-      -- Autoinstall languages that are not installed
-      auto_install = true,
-      highlight = {
-        enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
-      },
-      indent = { enable = true, disable = { 'ruby' } },
-    },
-    config = function(_, opts)
-      -- Prefer git instead of curl in order to improve connectivity in some environments
-      require('nvim-treesitter.install').prefer_git = true
-      ---@diagnostic disable-next-line: missing-fields
-      require('nvim-treesitter.configs').setup(opts)
-    end,
-  },
-  {
-    'nvim-tree/nvim-tree.lua',
-    config = function()
-      local nvim_tree = require('nvim-tree')
-
-      nvim_tree.setup({
-        view = {
-          width = {}
-        }
-      })
-    end
-  },
+  { 'nvim-tree/nvim-tree.lua' },
   {
     "iamcco/markdown-preview.nvim",
     cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
@@ -534,6 +490,7 @@ require("nvim-tree").setup({
     side = 'right',
     number = true,
     relativenumber = true,
+    adaptive_size = true,
   },
   renderer = {
     icons = {
