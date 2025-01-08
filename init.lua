6db2c7024705b7c6160ffc7d0f4d5c74d2609ed8
Alex Grant <alex.grant@shopify.com>
2025-01-08 11:15:22 -0500

    add harpoon

--- init.lua
+++ init.lua
@@ -439,7 +439,59 @@ require('lazy').setup({
       }
     }
   },
-  { 'preservim/nerdcommenter' }
+  { 'preservim/nerdcommenter' },
+  {
+    'ThePrimeagen/harpoon',
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    config = function()
+      local harpoon = require('harpoon').setup()
+      harpoon:setup({})
+
+      vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
+
+      -- Toggle previous & next buffers stored within Harpoon list
+      vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)
+      vim.keymap.set("n", "<C-S-N>", function() harpoon:list():next() end)
+
+      -- basic telescope configuration
+      local conf = require("telescope.config").values
+
+      local function toggle_telescope(harpoon_files)
+        local finder = function()
+          local file_paths = {}
+          for _, item in ipairs(harpoon_files.items) do
+            table.insert(file_paths, item.value)
+          end
+
+          return require("telescope.finders").new_table({
+            results = file_paths,
+          })
+        end
+
+        require("telescope.pickers").new({}, {
+          prompt_title = "Harpoon",
+          finder = finder(),
+          previewer = conf.file_previewer({}),
+          sorter = conf.generic_sorter({}),
+          attach_mappings = function(prompt_bufnr, map)
+            map("i", "<C-d>", function()
+                local state = require("telescope.actions.state")
+                local selected_entry = state.get_selected_entry()
+                local current_picker = state.get_current_picker(prompt_bufnr)
+
+                table.remove(harpoon_files.items, selected_entry.index)
+                current_picker:refresh(finder())
+            end)
+            return true
+        end,
+        }):find()
+      end
+
+      vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
+      { desc = "Open harpoon window" })
+    end,
+  }
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
